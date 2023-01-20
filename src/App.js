a40cbea7adb6caa370c8845753494ccfc3e859bb
Sixto Becerra <wow1sixto@gmail.com>
2023-01-19 23:24:05 -0500

    Se construye la pagina

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState, useEffect } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [searchTerm, setSearchTerm] = new useState("")
+    const [searchResult, setSearchResult] = new useState([])
+    const [error, setError] = new useState(null)
+    const [isLoading, setIsLoading] = new useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true)
+            try {
+                const res = await fetch(
+                    `https://api.api-ninjas.com/v1/celebrity?name=${searchTerm}`,
+                    {
+                        headers: {
+                            "X-Api-Key":
+                                "5Y5jKyHxrRxKoDDIhKJpjw==IUhio9nPMBSaeeCN",
+                        },
+                    }
+                )
+                const data = await res.json()
+                setSearchResult(data)
+            } catch (error) {
+                setError(error)
+            }
+            setIsLoading(false)
+        }
+        if (searchTerm.length > 0) {
+            fetchData()
+        }
+    }, [searchTerm, setError, setIsLoading, setSearchResult])
+
+    if (error) {
+        console.log(error)
+    }
+
+    return (
+        <div className="App">
+            <form>
+                <label>Search for a celebrity networth:</label>
+                <input
+                    type="text"
+                    id="search"
+                    value={searchTerm}
+                    onChange={(event) => setSearchTerm(event.target.value)}
+                />
+            </form>
+            {isLoading ? (
+                <p>One moment please...</p>
+            ) : (
+                searchResult.map((result, index) => {
+                    return (
+                        <div key={index}>
+                            <h2>{result.name}</h2>
+                            <h3>Nepo baby net worth: {result.net_worth}</h3>
+                        </div>
+                    )
+                })
+            )}
+        </div>
+    )
 }
 
-export default App;
+export default App
